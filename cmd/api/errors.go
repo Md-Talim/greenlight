c786c3b7d069469a96739cd052407352e5599a5b
Md. Talim <talimmohammad116@gmail.com>
2025-11-17 21:14:46 +0530

    refactor: add helper methods to manage errors

---
+++ cmd/api/errors.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// logError logs an error message along with the current request
+// method and URL as attributes in the log entry.
+func (app *application) logError(r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+}
+
+// errorResponse sends JSON-formatted error messages to the Client
+// with a given status code
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	res := envelop{"error": message}
+
+	err := app.writeJSON(w, status, res, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+// serverErrorResponse logs the detailed error message, then uses
+// errorResponse helper to send a 500 Internal Server Error status code and
+// JSON response (containing a generic error message) to the client
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+	message := "The server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+// notFoundResponse sends a 404 Not Found status code and JSON response to the client.
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+// methodNotAllowedResponse sends a 405 Method Not Allowed status code and JSON response to the client.
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
