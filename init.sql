02abcf72f03118064de8e5c79cd7e745d3caea2f
Md. Talim <talimmohammad116@gmail.com>
2025-11-18 21:40:15 +0530

    feat: set up postgresql with docker

---
+++ init.sql
@@ -0,0 +1,8 @@
+-- Create "greenlight" role with password auth
+CREATE ROLE greenlight WITH LOGIN PASSWORD 'pa$$word';
+
+-- Switch to the greenlight DB
+\c greenlight;
+
+-- Enable the citext extension
+CREATE EXTENSION IF NOT EXISTS citext;
